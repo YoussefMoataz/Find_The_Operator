b4bfcad83b918aa98593943f558c981c03f619fb
Youssef Moataz <yousefcrunshy03@gmail.com>
2021-12-15 21:57:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+
+    x = int(input("Enter first number\n"))
+    y = int(input("Enter second number\n"))
+
+    z = int(input("Enter the result\n"))
+
+    if (x + y) == z:
+        print(x, " + ", y, " = ", z)
+    elif (x - y) == z:
+        print(x, " - ", y, " = ", z)
+    elif (x * y) == z:
+        print(x, " * ", y, " = ", z)
+    elif (x / y) == z:
+        print(x, " / ", y, " = ", z)
+    elif (x % y) == z:
+        print(x, " % ", y, " = ", z)
+    elif (x ** y) == z:
+        print(x, " ^ ", y, " = ", z)
