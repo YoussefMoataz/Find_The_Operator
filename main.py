fa25765123385d13d8e714ca5640c3621e23a911
Youssef Moataz <yousefcrunshy03@gmail.com>
2021-12-15 22:00:36 +0200

    Ready

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 x = int(input("Enter first number\n"))
-    y = int(input("Enter second number\n"))
+y = int(input("Enter second number\n"))
 
-    z = int(input("Enter the result\n"))
+z = int(input("Enter the result\n"))
 
-    if (x + y) == z:
-        print(x, " + ", y, " = ", z)
-    elif (x - y) == z:
-        print(x, " - ", y, " = ", z)
-    elif (x * y) == z:
-        print(x, " * ", y, " = ", z)
-    elif (x / y) == z:
-        print(x, " / ", y, " = ", z)
-    elif (x % y) == z:
-        print(x, " % ", y, " = ", z)
-    elif (x ** y) == z:
-        print(x, " ^ ", y, " = ", z)
+if (x + y) == z:
+    print(x, " + ", y, " = ", z)
+elif (x - y) == z:
+    print(x, " - ", y, " = ", z)
+elif (x * y) == z:
+    print(x, " * ", y, " = ", z)
+elif (x / y) == z:
+    print(x, " / ", y, " = ", z)
+elif (x % y) == z:
+    print(x, " % ", y, " = ", z)
+elif (x ** y) == z:
+    print(x, " ^ ", y, " = ", z)
